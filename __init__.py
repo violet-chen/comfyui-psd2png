7c1051ebddf3864b53ab2c6f22f7cf5b51bace66
RuiChen <1505291171@qq.com>
2024-01-16 18:21:34 +0800

    取消print

--- __init__.py
+++ __init__.py
@@ -8,7 +8,6 @@ gun_pngs = os.listdir(os.path.join(current_path,"images"))
 gun_pngs_path = [os.path.join(current_path,"images",x) for x in gun_pngs]
 input_folder_path = folder_paths.get_input_directory()
 for gun_png_path in gun_pngs_path:
-    print(gun_png_path)
     gun_png_name = os.path.split(gun_png_path)[1]
     input_gun_png_path = os.path.join(input_folder_path,gun_png_name)
     if os.path.exists(input_gun_png_path):
